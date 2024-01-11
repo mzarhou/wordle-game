17724d80a1d2ce76662389edfe99a1ac2765cde4
mzarhou <mzarhou.cc@gmail.com>
2024-01-11 22:25:31 +0100

    fix: database url

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,7 +3,7 @@ import postgres from "postgres";
 import * as schema from "./schema";
 import { env } from "~/env";
 
-export const DATABASE_URL = `postgres://${env.POSTGRES_PASSWORD}:${env.POSTGRES_PASSWORD}@db:5432/${env.POSTGRES_DB}`;
+export const DATABASE_URL = `postgres://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}@db:5432/${env.POSTGRES_DB}`;
 export const queryClient = postgres(DATABASE_URL);
 
 export const db = drizzle(queryClient, { schema });
